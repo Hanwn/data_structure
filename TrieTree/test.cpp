ffb51e38626ff1ca9f0e5db8e3eefb52e128e33c
Hanwn <1934611881@qq.com>
2019-08-01 23:35:09 +0800

    update TrieTree

--- TrieTree/test.cpp
+++ TrieTree/test.cpp
@@ -3,3 +3,15 @@
 
 using namespace std;
 
+int main() {
+	Trie* t = new Trie();
+	t->insert("abc");
+	t->insert("a");
+	// t->insert("vbh");
+	if (t->find("a")) {
+		cout<<"exits";
+	}
+
+	return 0;
+}
+
